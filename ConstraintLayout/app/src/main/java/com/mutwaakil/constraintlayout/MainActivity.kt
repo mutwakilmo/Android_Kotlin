f4188437d71bc39061057e6a9ece8b85a89b512d
mutwakilmo <mutwakilmo@gmail.com>
2020-07-25 20:29:16 +0200

    Log d

--- ConstraintLayout/app/src/main/java/com/mutwaakil/constraintlayout/MainActivity.kt
+++ ConstraintLayout/app/src/main/java/com/mutwaakil/constraintlayout/MainActivity.kt
@@ -2,10 +2,21 @@ package com.mutwaakil.constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_apply.setOnClickListener {
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val  country = etCountry.text.toString()
+            Log.d("MainActivity","$firstName $lastName from $country")
+        }
     }
 }
