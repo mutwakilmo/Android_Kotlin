36b62ff877b52bf33e672777c1d8a29959c8f18b
mutwakilmo <mutwakilmo@gmail.com>
2020-07-25 18:54:19 +0200

    Basics layout

--- Tutorials/app/src/main/java/com/mutwaakil/tutorials/MainActivity.kt
+++ Tutorials/app/src/main/java/com/mutwaakil/tutorials/MainActivity.kt
@@ -2,10 +2,23 @@ package com.mutwaakil.tutorials
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       Log.d("MainActivity", "Hello This my first log messages")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart")
     }
 }
