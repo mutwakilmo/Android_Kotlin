dc7e6cd8fa9f6c6c472b7a0092b8545af8b0cf71
mutwakilmo <mutwakilmo@gmail.com>
2020-07-28 22:49:16 +0200

    set-up

---
+++ RecyclerView/app/src/main/java/com/mutwaakil/recyclerview/Todo.kt
@@ -0,0 +1,11 @@
+package com.mutwaakil.recyclerview
+
+/**
+ * Created by Mutwakil-Mo 🤩
+ * Android Engineer,
+ * Brussels
+ */
+data class Todo (
+    val title: String,
+    var isChecked: Boolean
+)
---
+++ RecyclerView/app/src/main/java/com/mutwaakil/recyclerview/TodoAdapter.kt
@@ -0,0 +1,13 @@
+package com.mutwaakil.recyclerview
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Created by Mutwakil-Mo 🤩
+ * Android Engineer,
+ * Brussels
+ */
+class TodoAdapter {
+    inner class TodoViewAdapter(itemView: View) :RecyclerView.ViewHolder(itemView)
+}
