9c350bddd7825302deb391f138088e347a86d982
mutwakilmo <mutwakilmo@gmail.com>
2020-07-29 12:38:35 +0200

    RECYCLERVIEW

--- RecyclerView/app/src/main/java/com/mutwaakil/recyclerview/MainActivity.kt
+++ RecyclerView/app/src/main/java/com/mutwaakil/recyclerview/MainActivity.kt
@@ -2,10 +2,33 @@ package com.mutwaakil.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var todoList = mutableListOf(
+            Todo("Follow AndroidDevs",false),
+            Todo("Feed my cat ",true),
+            Todo("Eat HelloFresh",true),
+            Todo("Learn Kotlin",true),
+            Todo("Learn Data structure and algorithm",false),
+            Todo("Take a shower",false)
+        )
+        val adapter = TodoAdapter(todoList)
+        rvTodos.adapter =adapter
+        rvTodos.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val title = etTodo.text.toString()
+            val todo = Todo(title, false)
+            todoList.add(todo)
+
+            adapter.notifyItemInserted(todoList.size -1)
+        }
     }
 }
--- RecyclerView/app/src/main/java/com/mutwaakil/recyclerview/TodoAdapter.kt
+++ RecyclerView/app/src/main/java/com/mutwaakil/recyclerview/TodoAdapter.kt
@@ -1,13 +1,34 @@
 package com.mutwaakil.recyclerview
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_todo.view.*
 
 /**
  * Created by Mutwakil-Mo 🤩
  * Android Engineer,
  * Brussels
  */
-class TodoAdapter {
-    inner class TodoViewAdapter(itemView: View) :RecyclerView.ViewHolder(itemView)
+class TodoAdapter (
+    var todos: List<Todo>
+) :RecyclerView.Adapter<TodoAdapter.TodoViewHolder>(){
+    inner class TodoViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+      val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
+        return TodoViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+            return  todos.size
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+         holder.itemView.apply {
+             tvTitle.text = todos[position].title
+             cbDone.isChecked = todos[position].isChecked
+         }
+    }
 }
