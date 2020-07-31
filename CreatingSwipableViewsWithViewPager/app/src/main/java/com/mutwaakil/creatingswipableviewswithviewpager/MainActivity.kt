5976b89581572d63663c991ed89e054b35e6f562
mutwakilmo <mutwakilmo@gmail.com>
2020-07-31 18:02:51 +0200

    TabLayout With ViewPager2

--- CreatingSwipableViewsWithViewPager/app/src/main/java/com/mutwaakil/creatingswipableviewswithviewpager/MainActivity.kt
+++ CreatingSwipableViewsWithViewPager/app/src/main/java/com/mutwaakil/creatingswipableviewswithviewpager/MainActivity.kt
@@ -2,7 +2,10 @@ package com.mutwaakil.creatingswipableviewswithviewpager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,5 +31,25 @@ class MainActivity : AppCompatActivity() {
 //        viewPager.beginFakeDrag()
 //        viewPager.fakeDragBy(-10f)
 //        viewPager.endFakeDrag()
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "Tab ${position + 1}"
+        }.attach()
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity,"Reselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity,"Unselected ${tab?.text}", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+               Toast.makeText(this@MainActivity,"Selected ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
